847dc9d698e4d2637e1996237c0f3481a20ee89b
Aghilesh <aghilesh4u@gmail.com>
2017-04-03 11:13:45 +0530

    test specs moved outside of app & PetComponent files are moved to pet directory inside app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { PetComponent } from './pet.component'
+import { PetComponent } from './pet/pet.component'
 
 @NgModule({
   declarations: [
