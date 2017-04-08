3d4e9a4601d033775a9e8505c729ea3ec1471d91
Aghilesh <aghilesh4u@gmail.com>
2017-04-08 15:13:29 +0530

    orderBy pipe added

---
+++ src/app/app.orderby.pipe.ts
@@ -0,0 +1,25 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+    name: 'orderby'
+})
+
+export class OrderByPipe implements PipeTransform {
+    transform(data: Array<any>,orderByKey:any, ascending:boolean=true) {
+        // return data.sort((a,b):any=>{
+        //     console.log('orderByKey',orderByKey)
+        //     return a.name>b[orderByKey];
+        // });
+        console.log('ascending',ascending);
+        return data.sort((prev:any, next:any):any=> {
+            if(prev[orderByKey] > next[orderByKey]){
+                return ascending ? true : false
+            }else if(prev[orderByKey] < next[orderByKey]){
+                return ascending ? false : true
+            }else{
+                return 0;
+            }
+            
+        })
+    }
+};
